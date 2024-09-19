5a20fc392cc3bb30298dd717ccae8bb87333e4d2
Myskip <myskip@163.com>
2024-09-19 22:38:07 +0800

    feature : Initial version 1.0.1

---
+++ RibbonBuild.cmake
@@ -0,0 +1,329 @@
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+macro(TargetInit)
+    set(SRC_DIRS "")
+    set(PUBLIC_HEADERS "")
+    set(INTERFACE_HEADERS "")
+    set(PRIVATE_HEADERS "")
+
+    set(DEPEND_COMPONENTS "")
+    set(DEPEND_LIBS "")
+    set(CUSTOM_CMAKE_C_FLAGS "")
+    set(CUSTOM_CMAKE_CXX_FLAGS "")
+    set(CUSTOM_CMAKE_EXE_LINKER_FLAGS "")
+    set(CUSTOM_DEFINES "")
+endmacro()
+
+macro(RibbonDF_target_config)
+    string(REPLACE " " ";" TARGET_CMAKE_C_FLAGS "${CMAKE_C_FLAGS};${CUSTOM_CMAKE_C_FLAGS}")
+    string(REPLACE " " ";" TARGET_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS};${CUSTOM_CMAKE_CXX_FLAGS}")
+    string(REPLACE " " ";" TARGET_CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS};${CUSTOM_CMAKE_EXE_LINKER_FLAGS}")
+    string(REPLACE " " ";" TARGET_DEFINES "${CUSTOM_DEFINES}")
+
+    message(STATUS "TARGET_CMAKE_EXE_LINKER_FLAGS: ${TARGET_CMAKE_EXE_LINKER_FLAGS}")
+    message(STATUS "TARGET_CMAKE_C_FLAGS: ${TARGET_CMAKE_C_FLAGS}")
+    message(STATUS "TARGET_CMAKE_CXX_FLAGS: ${TARGET_CMAKE_CXX_FLAGS}")
+    message(STATUS "TARGET_DEFINES: ${TARGET_DEFINES}")
+
+    foreach(PUBLIC_HEADER ${PUBLIC_HEADERS})
+        target_include_directories(${target_name} PUBLIC ${PUBLIC_HEADER})
+    endforeach(PUBLIC_HEADER ${PUBLIC_HEADERS})
+
+    message(STATUS "Public Headers: ${PUBLIC_HEADERS}")
+
+    foreach(INTERFACE_HEADER ${INTERFACE_HEADERS})
+        target_include_directories(${target_name} INTERFACE ${INTERFACE_HEADER})
+    endforeach(INTERFACE_HEADER ${INTERFACE_HEADERS})
+
+    message(STATUS "Interface Headers: ${INTERFACE_HEADERS}")
+
+    foreach(PRIVATE_HEADER ${PRIVATE_HEADERS})
+        target_include_directories(${target_name} PRIVATE ${PRIVATE_HEADER})
+    endforeach(PRIVATE_HEADER ${PRIVATE_HEADERS})
+
+    message(STATUS "Private Headers: ${PRIVATE_HEADERS}")
+
+    message(STATUS "Sources: ${SRC}")
+
+    foreach(depend_component ${DEPEND_COMPONENTS})
+        add_dependencies(${target_name} ${depend_component})
+        target_link_libraries(${target_name} PRIVATE ${depend_component})
+    endforeach()
+
+    message(STATUS "Depend components: ${DEPEND_COMPONENTS}")
+
+    foreach(depend_lib ${DEPEND_LIBS})
+        target_link_libraries(${target_name} PRIVATE ${depend_lib})
+    endforeach()
+
+    message(STATUS "Depend librarys: ${DEPEND_LIBS}")
+
+    target_compile_options(${target_name} PRIVATE "${TARGET_CMAKE_C_FLAGS}")
+    target_compile_options(${target_name} PRIVATE "${TARGET_CMAKE_CXX_FLAGS}")
+    target_link_options(${target_name} PRIVATE "${TARGET_CMAKE_EXE_LINKER_FLAGS}")
+    target_compile_definitions(${target_name} PRIVATE ${TARGET_DEFINES})
+
+    if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+        set_target_properties(${target_name} PROPERTIES
+            OUTPUT_NAME ${target_name}
+            DEBUG_POSTFIX "_debug"
+        )
+    endif()
+
+    install(TARGETS ${target_name}
+        RUNTIME DESTINATION bin
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib)
+
+    install(DIRECTORY ${PUBLIC_HEADERS} DESTINATION include/${target_name})
+    install(DIRECTORY ${INTERFACE_HEADERS} DESTINATION include/${target_name})
+endmacro()
+
+function(RibbonBuildApplication)
+    get_filename_component(target_name ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+
+    message("Register Application: [${target_name}]")
+
+    set(SRC "")
+
+    foreach(SRC_DIR ${SRC_DIRS})
+        aux_source_directory(${SRC_DIR} SRC)
+    endforeach(SRC_DIR ${SRC_DIRS})
+
+    add_executable(${target_name} ${SRC})
+
+    set_target_properties(${target_name} PROPERTIES INSTALL_RPATH "../lib")
+    set_target_properties(${target_name} PROPERTIES INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+    RibbonDF_target_config()
+endfunction()
+
+function(RibbonBuildComponent)
+    get_filename_component(target_name ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+
+    message("Register Component: [${target_name}]")
+
+    set(component_dynamic "STATIC")
+
+    if(ARGN)
+        list(GET ARGN 0 component_dynamic)
+    endif()
+
+    if(${component_dynamic} STREQUAL "DYNAMIC")
+        set(LIBRARY_TYPE SHARED)
+    else()
+        set(LIBRARY_TYPE STATIC)
+    endif()
+
+    message(STATUS "LIBRARY_TYPE: ${LIBRARY_TYPE}")
+
+    foreach(SRC_DIR ${SRC_DIRS})
+        aux_source_directory(${SRC_DIR} SRC)
+    endforeach(SRC_DIR ${SRC_DIRS})
+
+    add_library(${target_name} ${LIBRARY_TYPE} ${SRC})
+
+    RibbonDF_target_config()
+endfunction()
+
+function(ComponentEnable component_name value)
+    string(TOUPPER "CONFIG_MODULE_${component_name}_ENABLE" component_config)
+    set(${value} "${${component_config}}" PARENT_SCOPE)
+
+    # message(STATUS "${component_config}=${${component_config}}")
+endfunction()
+
+function(BuildTargets targets_path_list)
+    message("Build Targets start")
+
+    foreach(target_path ${targets_path_list})
+        get_filename_component(target_name ${target_path} NAME_WE)
+        ComponentEnable(${target_name} target_enable)
+
+        if(${target_enable})
+            message(STATUS "Target [${target_name}] enable")
+            TargetInit()
+            add_subdirectory(${target_path} "${PROJECT_SOURCE_DIR}/build/${target_name}")
+        else()
+            message(STATUS "Target [${target_name}] disabled")
+        endif()
+    endforeach()
+endfunction()
+
+macro(ConfigProject)
+    include(${PROJECT_SOURCE_DIR}/config/RibbonDFConfig.cmake)
+
+    # include_directories(${PROJECT_SOURCE_DIR}/config)
+    if(${CONFIG_PROJECT_DEBUG})
+        set(CMAKE_BUILD_TYPE Debug)
+    else()
+        set(CMAKE_BUILD_TYPE Release)
+    endif()
+
+    set(CMAKE_INSTALL_PREFIX "${PROJECT_SOURCE_DIR}/install" CACHE PATH "Install path" FORCE)
+
+    find_program(CMAKE_C_COMPILER "${CONFIG_TOOL_CHAIN_PREFIX}${CONFIG_C_COMPILER}")
+    find_program(CMAKE_CXX_COMPILER "${CONFIG_TOOL_CHAIN_PREFIX}${CONFIG_CXX_COMPILER}")
+
+    set(CMAKE_C_FLAGS "${CONFIG_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CONFIG_CXX_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CONFIG_LD_FLAGS}")
+
+    add_definitions(${PROJECT_DEFINES} "${CONFIG_PROJECT_DEFINES}")
+    message("Project Config")
+    message(STATUS "Project Install Path: ${CMAKE_INSTALL_PREFIX}")
+    message(STATUS "Project Build Type: ${CMAKE_BUILD_TYPE}")
+    message(STATUS "Project C_COMPILER: ${CMAKE_C_COMPILER}")
+    message(STATUS "Project CXX_COMPILER: ${CMAKE_CXX_COMPILER}")
+    message(STATUS "Project CMAKE_C_FLAGS: ${CMAKE_C_FLAGS}")
+    message(STATUS "Project CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
+    message(STATUS "Project CMAKE_EXE_LINKER_FLAGS: ${CMAKE_EXE_LINKER_FLAGS}")
+    message(STATUS "Project Defines: ${CONFIG_PROJECT_DEFINES}")
+endmacro()
+
+function(KconfigSetup targets)
+    message("Kconfig Setup")
+
+    # 入参改为项目路径和components路径
+    execute_process(
+        COMMAND python "${RibbonBuildPath}/RibbonKconfig.py" "-i${PROJECT_SOURCE_DIR};${RibbonComponentsPath}"
+        RESULT_VARIABLE return_code
+        OUTPUT_VARIABLE python_output
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    if(return_code EQUAL 0)
+        message(STATUS "Kconfig files generated")
+    else()
+        message(FATAL_ERROR "Kconfig files generate failed, return_code:${return_code}")
+    endif()
+
+    # 入参改为Kconfig文件路径
+    add_custom_target(menuconfig
+        COMMAND python "${RibbonBuildPath}/RibbonKconfig.py" "-m${PROJECT_SOURCE_DIR}"
+        COMMAND cd "${PROJECT_SOURCE_DIR}/build"
+        COMMAND cmake ".."
+        COMMENT "execute menuconfig"
+    )
+
+    add_custom_target(distclean
+        COMMAND python "${RibbonDF_PATH}/project/RibbonKconfig.py" "--distclean ${PROJECT_SOURCE_DIR}"
+        COMMENT "clean kconfig files"
+    )
+endfunction()
+
+# @brief 在指定路径下查找库
+# @param target_path 查找库的路径
+# @return
+function(ScanTarget target_path)
+    message("Scan Targets in PATH: ${target_path}")
+
+    # 遍历指定文件夹下的第一层文件夹下是否有CMakeLists.txt文件
+    file(GLOB scan_paths "${target_path}/*")
+
+    # message(STATUS "targets_list: ${targets_list}")
+    set(targets_list "")
+
+    foreach(scan_path ${scan_paths})
+        if(IS_DIRECTORY ${scan_path})
+            if(EXISTS "${scan_path}/CMakeLists.txt")
+                # get_filename_component(target_name ${scan_path} NAME)
+                list(APPEND targets_list ${scan_path})
+                message(STATUS "find target: ${scan_path}")
+            endif()
+        endif()
+    endforeach()
+
+    # targets中添加扫描到的
+    set(tmp_targets ${targets})
+    list(APPEND tmp_targets ${targets_list})
+    set(targets ${tmp_targets} PARENT_SCOPE)
+endfunction()
+
+function(DoBuild)
+    # 扫描目标
+    set(targets "")
+
+    foreach(target_path ${RibbonComponentsPath})
+        ScanTarget(${target_path})
+    endforeach(target_path ${RibbonComponentsPath})
+
+    if(NOT targets)
+        message(FATAL_ERROR "Not find any module, exit build.")
+    endif()
+
+    # kconfig生成
+    KconfigSetup("${targets}")
+
+    # 配置项目
+    ConfigProject()
+
+    # 注册目标
+    BuildTargets("${targets}")
+endfunction()
+
+function(RibbonDF_python_check)
+    find_package(Python 3.8 REQUIRED)
+
+    if(NOT Python_FOUND)
+        message(FATAL_ERROR "Python 3.8 or higher is required")
+    endif()
+endfunction()
+
+function(DependentsCheck)
+    message("Dependents Check")
+    RibbonDF_python_check()
+endfunction()
+
+function(VariablesCheck)
+    if(DEFINED RibbonBuildPath AND NOT RibbonBuildPath STREQUAL "")
+        # 将RibbonBuildPath转换为绝对路径
+        get_filename_component(RibbonBuildPath "${RibbonBuildPath}" ABSOLUTE)
+        message(STATUS "RibbonBuildPath: ${RibbonBuildPath}")
+    else()
+        message(FATAL_ERROR "RibbonBuildPath not defined or empty")
+    endif()
+
+    if(DEFINED RibbonComponentsPath AND NOT RibbonComponentsPath STREQUAL "")
+        # 将RibbonComponentsPath里的每一个成员转换为绝对路径
+        set(RibbonComponentsPathNew "")
+
+        foreach(path ${RibbonComponentsPath})
+            get_filename_component(path "${path}" ABSOLUTE)
+            list(APPEND RibbonComponentsPathNew ${path})
+        endforeach()
+
+        set(RibbonComponentsPath ${RibbonComponentsPathNew} PARENT_SCOPE)
+        message(STATUS "RibbonComponentsPath: ${RibbonComponentsPathNew}")
+    else()
+        message(FATAL_ERROR "RibbonComponentsPath not defined or empty")
+    endif()
+endfunction()
+
+# @brief 项目构建函数
+# @return
+function(RibbonBuild)
+    message("Project: ${PROJECT_NAME} Build Start")
+
+    VariablesCheck()
+
+    # 获取当前项目的根目录的绝对路径
+    # get_filename_component(RibbonDF_PATH "${PROJECT_SOURCE_DIR}/../.." ABSOLUTE)
+    # message("RibbonDF_PATH: ${RibbonDF_PATH}")
+
+    # # 引入TargetBuild.cmake库
+    # include(${RibbonDF_PATH}/project/TargetBuild.cmake)
+
+    # 设置项目的目标库路径列表
+    # set(RibbonComponentsPath "${RibbonDF_PATH}/third_lib" "${RibbonDF_PATH}/components" "${PROJECT_SOURCE_DIR}/src")
+
+    # 依赖检查
+    DependentsCheck()
+
+    # 构建项目
+    DoBuild()
+
+    message(STATUS "Project Config done")
+endfunction()
+
+RibbonBuild()
---
+++ RibbonKconfig.py
@@ -0,0 +1,127 @@
+import argparse
+import glob
+import os
+import shutil
+import kconfiglib
+from menuconfig import menuconfig
+
+
+def distclean(project_path):
+    shutil.rmtree(os.path.join(project_path, "build"))
+    shutil.rmtree(os.path.join(project_path, "config"))
+    os.mkdir(os.path.join(project_path, "build"))
+    os.mkdir(os.path.join(project_path, "config"))
+
+
+def write_cmake_setting(project_path, kconf):
+    # 遍历所有选项和配置
+    define_list = []
+
+    with open(os.path.join(project_path, "config/RibbonDFConfig.cmake"), "w",) as cmake_file:
+        cmake_file.write(
+            "#generated automatically by RibbonDF_build.py, do not modify it manually\n"
+        )
+        for sym in kconf.unique_defined_syms:
+            if sym.type == kconfiglib.BOOL:
+                cmake_value = "on" if sym.str_value == "y" else "off"
+                define_value = 1 if sym.str_value == "y" else 0
+                define_name = "-DCONFIG_" + sym.name + "=" + str(define_value)
+                define_list.append(define_name)
+            elif sym.type == kconfiglib.STRING:
+                cmake_value = '"' + sym.str_value + '"'
+                define_value = '"' + sym.str_value + '"'
+                define_name = "-DCONFIG_" + sym.name + "=" + define_value
+                define_list.append(define_name)
+            elif sym.type == kconfiglib.INT:
+                cmake_value = sym.str_value
+                define_value = int(sym.str_value)
+                define_name = "-DCONFIG_" + sym.name + "=" + str(define_value)
+                define_list.append(define_name)
+
+            # 生成CMake设置
+            cmake_setting = "set(CONFIG_{0} {1})\n".format(
+                sym.name, cmake_value)
+            cmake_file.write(cmake_setting)
+
+        define_str = " ".join(define_list)
+        cmake_file.write("set(PROJECT_DEFINES " + define_str + ")\n")
+
+
+def do_menuconfig(project_path):
+    kconf = kconfiglib.Kconfig(os.path.join(project_path, "build/Kconfig"))
+    kconf.load_config(os.path.join(project_path, "config/.config"))
+    menuconfig(kconf)
+    kconf.write_config(os.path.join(project_path, "config/.config"))
+    write_cmake_setting(project_path, kconf)
+
+
+def kconfig_load(project_path):
+    # 如果没有config目录，则创建
+    if not os.path.exists(os.path.join(project_path, "config")):
+        os.mkdir(os.path.join(project_path, "config"))
+
+    kconf = kconfiglib.Kconfig(os.path.join(project_path, "build/Kconfig"))
+    if (not os.path.exists(os.path.join(project_path, "config/.config"))):
+        kconf.write_config(os.path.join(project_path, "config/.config"))
+        write_cmake_setting(project_path, kconf)
+
+
+def Kconfig_init(project_path, components_path):
+    project_kconfig_file = os.path.join(project_path, "build/Kconfig")
+    current_path = os.path.dirname(os.path.abspath(__file__))
+
+    with open(project_kconfig_file, "w",) as kconfig_file:
+        kconfig_file.write(
+            "#generated automatically by RibbonDF_kconfig.py, do not modify it manually\n"
+        )
+        kconfig_file.write("menu \"project Configuration\"\n")
+        kconfig_file.write("source \"" + current_path + "/project.Kconfig\"\n")
+        kconfig_file.write("endmenu\n")
+
+        for component_path in components_path:
+            component_name = os.path.basename(component_path)
+            kconfig_file.write("menu \"" + component_name +
+                               " Configuration\"\n")
+            kconfig_file.write(
+                "osource \"" + component_path + "/*/Kconfig\"\n")
+            kconfig_file.write("endmenu\n")
+
+
+def project_init(project_path, components_path):
+    Kconfig_init(project_path, components_path)
+    kconfig_load(project_path)
+
+# @brief 解析命令行参数
+# @return 命令行参数对象
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--distclean", action="store", help="distclean")
+    parser.add_argument(
+        "-m", "--menuconfig", action="store", help="检查当前路径下的模块列表"
+    )
+    parser.add_argument(
+        "-i",
+        "--project_init",
+        type=str,
+        help="输出项目路径，和注册模块路径",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    if args.distclean:
+        distclean(args.distclean)
+    elif args.project_init:
+        init_args = args.project_init.split(";")
+        project_path = init_args[0]
+        components_path = init_args[1:]
+        project_init(project_path, components_path)
+    elif args.menuconfig:
+        do_menuconfig(args.menuconfig)
+    else:
+        print("未知命令")
+
+    exit(0)
